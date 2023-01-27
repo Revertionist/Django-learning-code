15ed5ceb4640c87b09b39954380dedf258d499ef
Govind R Nair <govindrnair.rev@gmail.com>
2023-01-27 20:26:04 +0530

    initial commit

---
+++ base/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+rooms = [
+    {'id': 1, 'name': 'this is a trial'},
+    {'id': 2, 'name': 'first few lines of django'},
+]
+
+
+def home(request):
+    return render(request, "index.html")
+
+def room(request):
+    return render(request, "index.html", {'rooms': rooms})
