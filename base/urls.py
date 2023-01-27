74fc9f32d81f8ca3258c4573dad8965289cb75c1
Govind R Nair <govindrnair.rev@ug.cusat.ac.in>
2023-01-27 22:47:45 +0530

    name change

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path ('', views.home),
-    path ('room/', views.room )
+    path('', views.home),
+    path('room/', views.room),
 ]
