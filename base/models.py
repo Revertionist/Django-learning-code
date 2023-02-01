2b07752ea618cc5e6f7e9c4e0e9f91da97e9a0d9
Govind R Nair <govindrnair.rev@ug.cusat.ac.in>
2023-02-01 23:14:31 +0530

    database update

--- base/models.py
+++ base/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50, null=False, blank=False)
+    author = models.CharField(max_length=20, null=False, blank=False)
+    stock = models.IntegerField(null=False, blank=False)
+    key = models.UUIDField(default=uuid.uuid4)
--- base/views.py
+++ base/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 rooms = [
     {'id': 1, 'name': 'this is a trial'},
